dbdf9c5a0d2957c2f75c9c1643ca31f76bee3a3e
JewelBreeveld <rjbreeveld@gmail.com>
2019-06-13 12:19:41 +0200

    Added componentDidMount method to fetch data from api

--- src/components/Dogslist.js
+++ src/components/Dogslist.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import * as request from "superagent";
 
 export default class Dogslist extends Component {
   state = { dogBreeds: null };
+
+  componentDidMount() {
+    request
+      .get("https://dog.ceo/api/breeds/list/all")
+      .then(console.log)
+      .catch(console.error);
+  }
+
   render() {
     return (
       <div className="dogs-list">
